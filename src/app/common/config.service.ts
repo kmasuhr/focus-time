98546025d3da8c607ff7c1af8d7db61e33fc2e64
Karol Masuhr <karol@masuhr.pl>
2018-12-15 00:00:24 +0100

    Add bed time

--- src/app/common/config.service.ts
+++ src/app/common/config.service.ts
@@ -13,19 +13,29 @@ export class ConfigService {
       message: 'Retro',
       color: '#7b1fa2',
     },
-
     {
       start: moment('07:00', this.TIME_FORMAT),
       end: moment('09:00', this.TIME_FORMAT),
       message: 'Focus time!',
       color: '#F44336',
     },
-
     {
       start: moment('12:00', this.TIME_FORMAT),
       end: moment('15:00', this.TIME_FORMAT),
       message: 'Focus time!',
       color: '#F44336',
+    },
+    {
+      start: moment('22:00', this.TIME_FORMAT),
+      end: moment('24:00', this.TIME_FORMAT),
+      message: 'Bed time!',
+      color: '#283593',
+    },
+    {
+      start: moment('00:00', this.TIME_FORMAT),
+      end: moment('06:00', this.TIME_FORMAT),
+      message: 'Bed time!',
+      color: '#283593',
     },
   ];
 
