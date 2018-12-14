f5af3a71f90ce39fd535f47cda321c10f80a7bc2
Karol Masuhr <karol@masuhr.pl>
2018-12-14 22:00:44 +0100

    Add styling

--- src/app/common/config.service.ts
+++ src/app/common/config.service.ts
@@ -8,7 +8,7 @@ export class ConfigService {
   TIME_FORMAT = 'HH:mm';
   focusTime = [
     {start: moment('10:00', this.TIME_FORMAT), end: moment('12:00', this.TIME_FORMAT)},
-    {start: moment('20:00', this.TIME_FORMAT), end: moment('22:00', this.TIME_FORMAT)},
+    // {start: moment('20:00', this.TIME_FORMAT), end: moment('22:00', this.TIME_FORMAT)},
   ];
 
   constructor() {
