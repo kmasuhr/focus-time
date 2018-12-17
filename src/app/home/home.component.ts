410a50d5e69808af867b6093ac9c9c961c851528
Karol Masuhr <karol@masuhr.pl>
2018-12-17 09:41:12 +0100

    Update message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,30 +3,30 @@ import {TimeService} from "../common/time.service";
 import {ConfigService} from "../common/config.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
   DEFAULT = {
-    message: 'Fun time!',
-    backgroundColor: '#8BC34A',
+    message: "Conversations are not prohibited. Enjoy.",
+    backgroundColor: "#8BC34A"
   };
   currentTime = null;
 
-  message = '';
-  backgroundColor = '';
+  message = "";
+  backgroundColor = "";
 
-  constructor(private _timeService: TimeService,
-              private _configService: ConfigService) {
-  }
-
+  constructor(
+    private _timeService: TimeService,
+    private _configService: ConfigService
+  ) {}
 
   ngOnInit() {
     this._timeService.getClock().subscribe(this.onTimeChange);
   }
 
-  onTimeChange = (value) => {
+  onTimeChange = value => {
     this.currentTime = value;
     this.updateFocusStatus();
   };
@@ -38,7 +38,7 @@ export class HomeComponent implements OnInit {
       if (this.currentTime.isBetween(event.start, event.end)) {
         this.message = event.message;
         this.backgroundColor = event.color;
-        return
+        return;
       }
     }
 
